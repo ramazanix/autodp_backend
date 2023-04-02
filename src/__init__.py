04a29bf8cc373ea38e7e10d79cef0bf1876acdfd
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-02 19:40:31 +0300

    Add initializator of FastAPI application

---
+++ src/__init__.py
@@ -0,0 +1,33 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from src.config import config as app_config
+from src.db import session_manager
+
+
+def init_app(init_db=True):
+    lifespan = None
+
+    if init_db:
+        session_manager.init(app_config.DB_URL)
+
+        @asynccontextmanager
+        async def lifespan(app: FastAPI):
+            yield
+            if session_manager._engine is not None:
+                await session_manager.close()
+
+    server = FastAPI(title="My FastAPI Server", lifespan=lifespan)
+
+    from .main import main_router, auth_jwt_exception_handler
+    from .auth.router import auth_router, users_router
+    from fastapi_jwt_auth.exceptions import AuthJWTException
+
+    server.include_router(main_router)
+    server.include_router(auth_router)
+    server.include_router(users_router)
+    server.add_exception_handler(AuthJWTException, auth_jwt_exception_handler)
+
+    return server
+
+
+server = init_app()
