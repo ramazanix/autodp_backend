d419ffbbc861c5cbbe90155826adc3566e1ece30
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-05 02:14:45 +0300

    Add update action to users

--- src/auth/services.py
+++ src/auth/services.py
@@ -3,7 +3,8 @@ from .models import User
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy import select
-from .schemas import UserSchemaCreate
+from sqlalchemy import update as sa_update
+from .schemas import UserSchemaCreate, UserSchemaUpdate
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -27,6 +28,27 @@ async def create(db: AsyncSession, user: UserSchemaCreate) -> User | None:
     return db_user
 
 
+async def update(
+    db: AsyncSession, payload: UserSchemaUpdate, user: User
+) -> User | None:
+    update_data = payload.dict(exclude_none=True, exclude_unset=True)
+    if update_data.get("password"):
+        hashed_passwd = get_password_hash(update_data.get("password"))
+        update_data["hashed_password"] = hashed_passwd
+        update_data.pop("password")
+
+    query = sa_update(User).where(User.username == user.username).values(update_data)
+    await db.execute(query)
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+
+async def delete(db: AsyncSession, user: User) -> None:
+    await db.delete(user)
+    await db.commit()
+
+
 async def get_with_paswd(db: AsyncSession, user: UserSchemaCreate) -> User | None:
     try:
         db_user = (
@@ -46,9 +68,8 @@ async def get(db: AsyncSession, username: str) -> User | None:
 
 
 async def get_all(db: AsyncSession, bound: int | None = None):
-    return (await db.execute(select(User).limit(bound))).scalars().all()
-
-
-async def delete(db: AsyncSession, user: User) -> None:
-    await db.delete(user)
-    await db.commit()
+    return (
+        (await db.execute(select(User).limit(bound).order_by(User.created_at)))
+        .scalars()
+        .all()
+    )
