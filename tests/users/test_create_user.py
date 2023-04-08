b5ecf245203b73798a4cb10d24f9ac3583b01e3f
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-09 01:35:58 +0300

    Add some test cases and docstrings

--- tests/users/test_create_user.py
+++ tests/users/test_create_user.py
@@ -6,53 +6,152 @@ from .schemas import user, users
 
 @pytest.mark.asyncio
 async def test_create_user(client: AsyncClient):
-    response = await client.post('/users/',
-                                 json={'username': 'Paul', 'password': 'paul_password'})
+    """
+    Trying to create user
+    """
+    response = await client.post(
+        "/users/", json={"username": "Paul", "password": "paul_password"}
+    )
     assert response.status_code == 201
 
     assert exact_schema(user) == response.json()
 
-    assert response.json().get('username') == 'Paul'
+    assert response.json().get("username") == "Paul"
 
 
 @pytest.mark.asyncio
 async def test_create_couple_users(client: AsyncClient):
-    response = await client.post('/users/',
-                                 json={'username': 'Joe', 'password': 'joe_password'})
+    """
+    Trying to create a couple of users
+    """
+    response = await client.post(
+        "/users/", json={"username": "Joe", "password": "joe_password"}
+    )
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
-    assert response.json().get('username') == 'Joe'
+    assert response.json().get("username") == "Joe"
 
-    response = await client.post('/users/',
-                                 json={'username': 'Tom', 'password': 'tom_password'})
+    response = await client.post(
+        "/users/", json={"username": "Tom", "password": "tom_password"}
+    )
 
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
-    assert response.json().get('username') == 'Tom'
+    assert response.json().get("username") == "Tom"
 
-    response = await client.get('/users/')
+    response = await client.get("/users/")
     assert response.status_code == 200
     assert len(response.json()) == 2
     assert exact_schema(users) == response.json()
 
 
 @pytest.mark.asyncio
-async def test_create_blank_user_data(client: AsyncClient):
-    response = await client.post('/users/', json={})
+async def test_create_blank_body(client: AsyncClient):
+    """
+    Trying to send request with blank body
+    """
+    response = await client.post("/users/", json={})
     assert response.status_code == 422
 
 
-    # response = await client.post('/users/', json={'a': 'b'})
-    # assert response.status_code == 422
-    #
-    # response = await client.post('/users/', json={'username': ''})
-    # assert response.status_code == 422
-    #
-    # response = await client.post('/users/', json={'password': ''})
-    # assert response.status_code == 422
-    #
-    # response = await client.post('/users/', json={'username': '', 'password': ''})
-    # assert response.status_code == 422
-    #
-    # response = await client.post('/users/', json={'username': 'Alex', 'password': '123'})
-    # assert response.status_code == 422
+@pytest.mark.asyncio
+async def test_create_invalid_user_data(client: AsyncClient):
+    """
+    Trying to send invalid body content
+    """
+    response = await client.post("/users/", json={"a": "b"})
+    assert response.status_code == 422
+
+    response = await client.post("/users/", json={"username": ""})
+    assert response.status_code == 422
+
+    response = await client.post("/users/", json={"password": ""})
+    assert response.status_code == 422
+
+    response = await client.post("/users/", json={"username": "", "password": ""})
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_short_username(client: AsyncClient):
+    """
+    Trying to create user with too short username
+    """
+    response = await client.post(
+        "/users/", json={"username": "u", "password": "password"}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_long_username(client: AsyncClient):
+    """
+    Trying to create user with too long username
+    """
+    response = await client.post(
+        "/users/", json={"username": "me" * 21, "password": "password"}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_short_password(client: AsyncClient):
+    """
+    Trying to create user with too short password
+    """
+    response = await client.post(
+        "/users/", json={"username": "Alex", "password": "123"}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_long_password(client: AsyncClient):
+    """
+    Trying to create user with too long password
+    """
+    response = await client.post(
+        "/users/", json={"username": "Alex", "password": "123" * 11}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_short_fields(client: AsyncClient):
+    """
+    Trying to create user with too short username and password
+    """
+    response = await client.post(
+        "/users/", json={"username": "u", "password": "passwd"}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_user_long_fields(client: AsyncClient):
+    """
+    Trying to create user with too long username and password
+    """
+    response = await client.post(
+        "/users/", json={"username": "Joe" * 10, "password": "passwd" * 6}
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_existing_user(client: AsyncClient):
+    """
+    Trying to create existing user
+    """
+    response = await client.post(
+        "/users/", json={"username": "Alex", "password": "alex_password"}
+    )
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "Alex"
+
+    response = await client.post(
+        "/users/", json={"username": "Alex", "password": "some_password"}
+    )
+    assert response.status_code == 400
+    assert response.json().get("detail") == "User already exists"
