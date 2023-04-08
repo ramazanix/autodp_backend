c43a2e6d843c2af64101fc21c553e965d4051a17
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-09 02:40:36 +0300

    Add functions for testing

--- src/db.py
+++ src/db.py
@@ -54,6 +54,13 @@ class DatabaseSessionManager:
         finally:
             await session.close()
 
+    # For testing
+    async def create_all(self, connection: AsyncConnection):
+        await connection.run_sync(Base.metadata.create_all)
+
+    async def drop_all(self, connection: AsyncConnection):
+        await connection.run_sync(Base.metadata.drop_all)
+
 
 session_manager = DatabaseSessionManager()
 
