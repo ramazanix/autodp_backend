64e7a734787b1958fb52075affa357ead8e6bd84
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-09 01:32:16 +0300

    Reformat code with black

--- tests/users/schemas.py
+++ tests/users/schemas.py
@@ -1,8 +1,3 @@
-user = {
-    'id': str,
-    'username': str,
-    'created_at': str,
-    'updated_at': str
-}
+user = {"id": str, "username": str, "created_at": str, "updated_at": str}
 
-users = [user]
+users: list[user] = [user]
