5cd7bffff35b73502701ed17bb3b679a2774b645
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-11 01:06:29 +0300

    Reformat code with black

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -7,19 +7,21 @@ from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_auth_not_existed_user(client: AsyncClient):
-    response = await client.post('/auth/login/', json={'username': 'not_exists', 'password': '12345678'})
+    response = await client.post(
+        "/auth/login/", json={"username": "not_exists", "password": "12345678"}
+    )
     assert response.status_code == 401
-    assert response.json().get('detail') == 'Bad username or password'
+    assert response.json().get("detail") == "Bad username or password"
 
 
 @pytest.mark.asyncio
 async def test_auth_existed_user(client: AsyncClient):
-    user_data = {'username': 'Alex', 'password': 'alex_password'}
-    response = await client.post('/users/', json=user_data)
+    user_data = {"username": "Alex", "password": "alex_password"}
+    response = await client.post("/users/", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
-    assert response.json().get('username') == 'Alex'
+    assert response.json().get("username") == "Alex"
 
-    response = await client.post('/auth/login/', json=user_data)
+    response = await client.post("/auth/login/", json=user_data)
     assert response.status_code == 200
     assert exact_schema(login) == response.json()
