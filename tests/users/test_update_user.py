d696d5ee811d032144e0a534217d6347d07fe895
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-10 14:42:22 +0300

    Add a few tests

--- tests/users/test_update_user.py
+++ tests/users/test_update_user.py
@@ -71,4 +71,92 @@ async def test_update_user_blank_body(client: AsyncClient):
 
     response = await client.patch("/users/Alex/", json={}, headers=headers)
 
+    assert response.status_code == 400
+
+
+@pytest.mark.asyncio
+async def test_update_user_too_small_username(client: AsyncClient):
+    """
+    Trying to update user's username to small value
+    """
+    user_data = {"username": "Joe", "password": "joe_password"}
+    response = await client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "Joe"
+
+    response = await client.post("/auth/login/", json=user_data)
+    assert response.status_code == 200
+    access_token = response.json().get("access_token")
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        "/users/Joe/", json={"username": "J"}, headers=headers
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_update_user_too_long_username(client: AsyncClient):
+    """
+    Trying to update user's username to long value
+    """
+    user_data = {"username": "Alex", "password": "alex_password"}
+    response = await client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "Alex"
+
+    response = await client.post("/auth/login/", json=user_data)
+    assert response.status_code == 200
+    access_token = response.json().get("access_token")
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        "/users/Alex/", json={"username": "A" * 21}, headers=headers
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_update_user_too_small_password(client: AsyncClient):
+    """
+    Trying to update user's password to small value
+    """
+    user_data = {"username": "Alex", "password": "alex_password"}
+    response = await client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "Alex"
+
+    response = await client.post("/auth/login/", json=user_data)
+    assert response.status_code == 200
+    access_token = response.json().get("access_token")
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        "/users/Alex/", json={"password": "A" * 33}, headers=headers
+    )
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_update_user_too_long_password(client: AsyncClient):
+    """
+    Trying to update user's password to long value
+    """
+    user_data = {"username": "Alex", "password": "alex_password"}
+    response = await client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    assert exact_schema(user) == response.json()
+    assert response.json().get("username") == "Alex"
+
+    response = await client.post("/auth/login/", json=user_data)
+    assert response.status_code == 200
+    access_token = response.json().get("access_token")
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = await client.patch(
+        "/users/Alex/", json={"password": "A" * 33}, headers=headers
+    )
     assert response.status_code == 422
