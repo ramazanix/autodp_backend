3e133ce45ccee2aba5f5527e663666decd52633b
Ramazan <70964774+Ramazan2002@users.noreply.github.com>
2023-04-12 17:44:17 +0300

    Refactoring auth module to hold tokens in cookie

--- tests/users/test_create_user.py
+++ tests/users/test_create_user.py
@@ -4,19 +4,22 @@ from pytest_schema import exact_schema
 from .schemas import user, users
 
 
+user_data = {"username": "Paul", "password": "paul_password"}
+
+
 @pytest.mark.asyncio
 async def test_create_user(client: AsyncClient):
     """
     Trying to create user
     """
     response = await client.post(
-        "/users/", json={"username": "Paul", "password": "paul_password"}
+        "/users/", json=user_data
     )
     assert response.status_code == 201
 
     assert exact_schema(user) == response.json()
 
-    assert response.json().get("username") == "Paul"
+    assert response.json().get("username") == user_data['username']
 
 
 @pytest.mark.asyncio
@@ -25,11 +28,11 @@ async def test_create_couple_users(client: AsyncClient):
     Trying to create a couple of users
     """
     response = await client.post(
-        "/users/", json={"username": "Joe", "password": "joe_password"}
+        "/users/", json=user_data
     )
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
-    assert response.json().get("username") == "Joe"
+    assert response.json().get("username") == user_data['username']
 
     response = await client.post(
         "/users/", json={"username": "Tom", "password": "tom_password"}
@@ -144,14 +147,14 @@ async def test_create_existing_user(client: AsyncClient):
     Trying to create existing user
     """
     response = await client.post(
-        "/users/", json={"username": "Alex", "password": "alex_password"}
+        "/users/", json=user_data
     )
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
-    assert response.json().get("username") == "Alex"
+    assert response.json().get("username") == user_data['username']
 
     response = await client.post(
-        "/users/", json={"username": "Alex", "password": "some_password"}
+        "/users/", json=user_data
     )
     assert response.status_code == 400
     assert response.json().get("detail") == "User already exists"
