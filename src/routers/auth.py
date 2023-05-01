7f8bd2b11d0c8ad701ec92331a5157f2dfe3a408
Ramazan2002 <ramazanui2002@gmail.com>
2023-05-01 23:57:18 +0300

    Refactor code to work with redis storage

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from ..config import settings
 from ..db import get_db
 from fastapi_jwt_auth import AuthJWT
-from ..services.user import get_with_paswd
+from ..services.user import get_with_paswd, get_by_id
 from redis import Redis
 
 auth_router = APIRouter(prefix="/auth", tags=["Authenticate"])
@@ -35,19 +35,33 @@ async def login(
     if not db_user:
         raise HTTPException(status_code=401, detail="Bad username or password")
 
-    access_token = authorize.create_access_token(subject=user.username)
-    refresh_token = authorize.create_refresh_token(subject=user.username)
+    user_claims = {"user_claims": {"id": str(db_user.id)}}
+    access_token = authorize.create_access_token(
+        subject=user.username, user_claims=user_claims
+    )
+    refresh_token = authorize.create_refresh_token(
+        subject=user.username, user_claims=user_claims
+    )
     authorize.set_access_cookies(access_token)
     authorize.set_refresh_cookies(refresh_token)
-    return {"success": "Successfully login"}
+    return {"success": "Successfully logged in"}
 
 
 @auth_router.post("/refresh")
-def refresh_access_token(authorize: AuthJWT = Depends()):
+async def refresh_access_token(
+    db: AsyncSession = Depends(get_db), authorize: AuthJWT = Depends()
+):
     authorize.jwt_refresh_token_required()
+    user_claims = authorize.get_raw_jwt()["user_claims"]
+    jti = authorize.get_raw_jwt()["jti"]
+    user_id = user_claims["id"]
 
-    current_user = authorize.get_jwt_subject()
-    new_access_token = authorize.create_access_token(subject=current_user)
+    current_user = await get_by_id(db, user_id)
+
+    new_access_token = authorize.create_access_token(
+        subject=current_user.username, user_claims=user_claims
+    )
+    redis_conn.setex(jti, settings.AUTHJWT_COOKIE_MAX_AGE, "true")
     authorize.set_access_cookies(new_access_token)
     return {"success": "The token has been refreshed"}
 
@@ -55,5 +69,7 @@ def refresh_access_token(authorize: AuthJWT = Depends()):
 @auth_router.delete("/logout")
 async def logout(authorize: AuthJWT = Depends()):
     authorize.jwt_required()
+    jti = authorize.get_raw_jwt()["jti"]
+    redis_conn.setex(jti, settings.AUTHJWT_COOKIE_MAX_AGE, "true")
     authorize.unset_jwt_cookies()
     return {"success": "Successfully logout"}
