e0b47fe44a33d60dd0e569b62fd6f5b0b78452f7
Ramazan2002 <ramazanui2002@gmail.com>
2023-05-03 00:23:32 +0300

    Remove slash from the end of path

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -21,7 +21,7 @@ async def test_auth_not_existed_user(client: AsyncClient):
     Trying to authenticate not existed user
     """
     response = await client.post(
-        "/auth/login/", json={"username": "not_exists", "password": "12345678"}
+        "/auth/login", json={"username": "not_exists", "password": "12345678"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
@@ -32,12 +32,12 @@ async def test_auth_existed_user(client: AsyncClient):
     """
     Trying to authenticate existed user
     """
-    response = await client.post("/users/", json=user_data)
+    response = await client.post("/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
-    response = await client.post("/auth/login/", json=user_data)
+    response = await client.post("/auth/login", json=user_data)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
     assert list(response.cookies.keys()) == correct_cookies
@@ -48,7 +48,7 @@ async def test_auth_blank_body(client: AsyncClient):
     """
     Trying to authenticate user with incorrect blank data
     """
-    response = await client.post("/auth/login/", json={})
+    response = await client.post("/auth/login", json={})
     assert response.status_code == 422
 
 
@@ -57,30 +57,30 @@ async def test_auth_not_valid_data(client: AsyncClient):
     """
     Trying to authenticate user with invalid data
     """
-    response = await client.post("/users/", json=user_data)
+    response = await client.post("/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
     response = await client.post(
-        "/auth/login/", json={"username": "Sam", "password": "123"}
+        "/auth/login", json={"username": "Sam", "password": "123"}
     )
     assert response.status_code == 422
 
     response = await client.post(
-        "/auth/login/", json={"username": "Sam", "password": "12345678"}
+        "/auth/login", json={"username": "Sam", "password": "12345678"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
     response = await client.post(
-        "/auth/login/", json={"username": "1234", "password": "123456789"}
+        "/auth/login", json={"username": "1234", "password": "123456789"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
 
     response = await client.post(
-        "/auth/login/", json={"username": "1234", "password": "sam_password"}
+        "/auth/login", json={"username": "1234", "password": "sam_password"}
     )
     assert response.status_code == 401
     assert response.json().get("detail") == "Bad username or password"
@@ -91,22 +91,22 @@ async def test_refresh_access_token(client: AsyncClient):
     """
     Trying to refresh access token
     """
-    response = await client.post("/users/", json=user_data)
+    response = await client.post("/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
-    response = await client.post("/auth/login/", json=user_data)
+    response = await client.post("/auth/login", json=user_data)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
     assert list(response.cookies.keys()) == correct_cookies
 
     headers = {"X-CSRF-Token": response.cookies.get("csrf_refresh_token")}
-    response = await client.post("/auth/refresh/", headers=headers)
+    response = await client.post("/auth/refresh", headers=headers)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
 
-    response = await client.post("/auth/refresh/")
+    response = await client.post("/auth/refresh")
     assert response.status_code == 401
     assert exact_schema(error) == response.json()
     assert response.json().get("detail") == "Missing CSRF Token"
@@ -117,25 +117,25 @@ async def test_logout(client: AsyncClient):
     """
     Trying to log out
     """
-    response = await client.post("/users/", json=user_data)
+    response = await client.post("/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
-    response = await client.post("/auth/login/", json=user_data)
+    response = await client.post("/auth/login", json=user_data)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
     assert list(response.cookies.keys()) == correct_cookies
 
     cookies = response.cookies
 
-    response = await client.delete("/auth/logout/")
+    response = await client.delete("/auth/logout")
     assert response.status_code == 401
     assert exact_schema(error) == response.json()
     assert response.json().get("detail") == "Missing CSRF Token"
 
     headers = {"X-CSRF-Token": cookies.get("csrf_access_token")}
-    response = await client.delete("/auth/logout/", headers=headers)
+    response = await client.delete("/auth/logout", headers=headers)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
     assert len(response.cookies) == 0
@@ -146,20 +146,20 @@ async def test_get_current_user(client: AsyncClient):
     """
     Trying to get current user
     """
-    response = await client.get("/user/")
+    response = await client.get("/user")
     assert response.status_code == 401
     assert exact_schema(error)
 
-    response = await client.post("/users/", json=user_data)
+    response = await client.post("/users", json=user_data)
     assert response.status_code == 201
     assert exact_schema(user) == response.json()
     assert response.json().get("username") == user_data["username"]
 
-    response = await client.post("/auth/login/", json=user_data)
+    response = await client.post("/auth/login", json=user_data)
     assert response.status_code == 200
     assert exact_schema(success) == response.json()
     assert list(response.cookies.keys()) == correct_cookies
 
-    response = await client.get("/user/")
+    response = await client.get("/user")
     assert response.status_code == 200
     assert exact_schema(user)
