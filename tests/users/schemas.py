6581e23861a64f1bfdd7019be0bd6fee3eef818f
Ramazan2002 <ramazanui2002@gmail.com>
2023-05-16 17:49:59 +0300

    Define user_base list

--- tests/users/schemas.py
+++ tests/users/schemas.py
@@ -1,3 +1,11 @@
-user = {"id": str, "username": str, "created_at": str, "updated_at": str}
+user_base = {"name": str}
+user = {
+    "id": str,
+    "username": str,
+    "created_at": str,
+    "updated_at": str,
+    "role": {"name": str, "description": str},
+}
 
+users_base: list[user_base] = [user_base]
 users: list[user] = [user]
