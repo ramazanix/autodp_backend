182b41ed77a73eeb19e1d8b5eb3d95814493d468
Ramazan2002 <ramazanui2002@gmail.com>
2023-06-02 23:48:52 +0300

    Add routes to current user and admin

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,8 +1,15 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Query
+from ..routers import admin_router
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..schemas.user import UserSchemaCreate, UserSchema, UserSchemaUpdate
+from ..schemas.user import (
+    UserSchemaCreate,
+    UserSchema,
+    UserSchemaUpdate,
+    UserSchemaUpdateAdmin,
+)
 from ..services.user import create, update, delete, get_all, get_by_username
+from ..services.role import get_by_name
 from ..config import settings
 from ..db import get_db
 from ..dependencies import Auth, auth_checker
@@ -21,22 +28,24 @@ async def get_current_user(
     return await authorize.get_current_user(db)
 
 
-@users_router.post("", response_model=UserSchema, status_code=201, tags=["Admin"])
+@admin_router.post("/users", response_model=UserSchema, status_code=201)
+@users_router.post("", response_model=UserSchema, status_code=201)
 async def create_user(
     user: UserSchemaCreate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
-    if user.username == "me":
+    if user.username in ["me", "super_user"]:
         raise HTTPException(status_code=400, detail="Not allowed username")
     new_user = await create(db, user)
+
     if not new_user:
         raise HTTPException(status_code=400, detail="User already exists")
     return new_user
 
 
-@users_router.patch("/{username}", response_model=UserSchema, tags=["Admin"])
+@admin_router.patch("/users/{username}", response_model=UserSchema)
 async def update_user(
     username: str,
-    payload: UserSchemaUpdate,
+    payload: UserSchemaUpdateAdmin,
     db: Annotated[AsyncSession, Depends(get_db)],
     authorize: Annotated[Auth, Depends(auth_checker)],
 ):
@@ -51,10 +60,16 @@ async def update_user(
     new_user_data: dict = payload.dict()
     if not any(new_user_data.values()):
         raise HTTPException(status_code=400)
+
+    if role_name := new_user_data.get("role_name"):
+        db_role = await get_by_name(db, role_name)
+        if not db_role:
+            raise HTTPException(status_code=400, detail="Role not found")
+
     return await update(db, payload, existed_user)
 
 
-@users_router.delete("/{username}", status_code=204, tags=["Admin"])
+@admin_router.delete("/users/{username}", status_code=204)
 async def delete_user(
     username: str,
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -70,7 +85,8 @@ async def delete_user(
     return await delete(db, existed_user)
 
 
-@users_router.get("", response_model=list[UserSchema], tags=["Admin"])
+@admin_router.get("/users", response_model=list[UserSchema])
+@users_router.get("", response_model=list[UserSchema])
 async def get_all_users(
     db: Annotated[AsyncSession, Depends(get_db)],
     limit: int | None = Query(None, gt=0),
@@ -78,11 +94,11 @@ async def get_all_users(
     return await get_all(db, limit)
 
 
+@admin_router.get(
+    "/users/{username}", response_model=UserSchema, dependencies=[Depends(auth_checker)]
+)
 @users_router.get(
-    "/{username}",
-    response_model=UserSchema,
-    dependencies=[Depends(auth_checker)],
-    tags=["Admin"],
+    "/{username}", response_model=UserSchema, dependencies=[Depends(auth_checker)]
 )
 async def get_user(username: str, db: Annotated[AsyncSession, Depends(get_db)]):
     user = await get_by_username(db, username=username)
