53bda41e6ab5f3ed6da9d1ce70e383deb35105e4
Ramazan2002 <ramazanui2002@gmail.com>
2023-06-07 23:46:56 +0300

    Add path to get post by id and update post

--- src/routers/post.py
+++ src/routers/post.py
@@ -1,10 +1,10 @@
 from typing import Annotated
+from pydantic import UUID4
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..routers import admin_router
-from ..schemas.post import PostSchema, PostSchemaCreate
-from ..services.post import get_all, create
-from ..services.user import get_by_id as get_user_by_id
+from ..schemas.post import PostSchema, PostSchemaCreate, PostSchemaUpdate
+from ..services.post import get_all, get_by_id, create, update
 from ..db import get_db
 from ..dependencies import Auth, auth_checker
 
@@ -16,10 +16,15 @@ posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 @posts_router.get("", response_model=list[PostSchema])
 async def get_all_posts(
     db: Annotated[AsyncSession, Depends(get_db)],
-    authorize: Annotated[Auth, Depends(auth_checker)],
     limit: int | None = Query(None, gt=0),
 ):
     return await get_all(db, limit)
+
+
+@admin_router.get("/posts/{post_id}", response_model=PostSchema)
+@posts_router.get("/{post_id}", response_model=PostSchema)
+async def get_post(post_id: UUID4, db: Annotated[AsyncSession, Depends(get_db)]):
+    return await get_by_id(db, post_id)
 
 
 @admin_router.post("/posts", response_model=PostSchema, status_code=201)
@@ -32,3 +37,22 @@ async def create_post(
     current_user = await authorize.get_current_user(db)
     owner_id = current_user.id
     return await create(db, post, owner_id)
+
+
+@admin_router.patch("/posts/{post_id}", response_model=PostSchema)
+@posts_router.patch("/{post_id}", response_model=PostSchema)
+async def update_post(
+    post_id: UUID4,
+    payload: PostSchemaUpdate,
+    db: Annotated[AsyncSession, Depends(get_db)],
+    authorize: Annotated[Auth, Depends(auth_checker)],
+):
+    db_post = await get_by_id(db, post_id)
+    if not db_post:
+        raise HTTPException(status_code=400, detail="Post not found")
+
+    new_post_data: dict = payload.dict()
+    if not any(new_post_data.values()):
+        raise HTTPException(status_code=400)
+
+    return await update(db, payload, db_post)
