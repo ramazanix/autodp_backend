1b364dc4a6fd3d7d7f0f4e25c973165525859b7b
Ramazan2002 <ramazanui2002@gmail.com>
2023-06-07 23:48:21 +0300

    Add method to update post by id and refactor method to get post by id

--- src/services/post.py
+++ src/services/post.py
@@ -4,15 +4,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from collections.abc import Sequence
 from sqlalchemy import select as sa_select
 from sqlalchemy import update as sa_update
-from ..schemas.post import PostSchemaCreate
+from ..schemas.post import PostSchemaCreate, PostSchemaUpdate
 
 
 async def get_all(db: AsyncSession, bound: int | None = None) -> Sequence[Post]:
     return (await db.execute(sa_select(Post).limit(bound))).scalars().all()
 
 
-async def get_by_id(db: AsyncSession, id: UUID4) -> Post | None:
-    return await db.execute(sa_select(Post).where(Post.id))
+async def get_by_id(db: AsyncSession, post_id: UUID4) -> Post | None:
+    return await db.get(Post, post_id)
 
 
 async def create(db: AsyncSession, post: PostSchemaCreate, owner_id: UUID4) -> Post:
@@ -21,3 +21,12 @@ async def create(db: AsyncSession, post: PostSchemaCreate, owner_id: UUID4) -> P
     await db.commit()
     await db.refresh(db_post)
     return db_post
+
+
+async def update(db: AsyncSession, payload: PostSchemaUpdate, post: Post) -> Post:
+    update_data = payload.dict(exclude_none=True, exclude_unset=True)
+    query = sa_update(Post).where(Post.id == post.id).values(update_data)
+    await db.execute(query)
+    await db.commit()
+    await db.refresh(post)
+    return post
